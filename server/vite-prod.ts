47381f88c50f3e2e7da736896f726709ae776062
youssefe-sa <sahabyoussef@gmail.com>
2025-07-16 13:48:04 +0200

    Update vite-prod.ts

--- server/vite-prod.ts
+++ server/vite-prod.ts
@@ -1,1 +1,12 @@
+// server/vite-prod.ts
+import { type Express } from "express";
+import path from "path";
+
+export function serveStatic(app: Express) {
+  const dist = path.resolve(__dirname, "../client/dist");
+  app.use(express.static(dist));
+  app.use("*", (_req, res) => {
+    res.sendFile(path.resolve(dist, "index.html"));
+  });
+}
 
